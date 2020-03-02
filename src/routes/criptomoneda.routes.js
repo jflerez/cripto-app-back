935bc6dab4d3c31d179f5b20171e5780a8af7388
Jonathan Flerez <jhonatanflerez@gmail.com>
2020-03-02 01:53:42 -0500

    fix router list moneda top 3

--- src/routes/criptomoneda.routes.js
+++ src/routes/criptomoneda.routes.js
@@ -5,7 +5,7 @@ const {validateSaveCriptomoneda} = require("../validation/request.criptomoneda")
 module.exports = ({CriptomonedaController})=>{
 const router = Router();
 
-router.get("/:usuarioId", AuthMiddleware, CriptomonedaController.getCriptomonedasByUsuario);
+//router.get("/:usuarioId", AuthMiddleware, CriptomonedaController.getCriptomonedasByUsuario);
 router.get("/top", AuthMiddleware, CriptomonedaController.getCriptomonedasTopByUsuario);
 router.post("/agregar",AuthMiddleware,validateSaveCriptomoneda, CriptomonedaController.createCriptomoneda);
 return router;
